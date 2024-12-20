642eed53b8b0eff5cb0bb5850a51e198d3dcb1e2
Zydex <uzdenbekove@gmail.com>
2024-12-20 20:30:25 +0600

    Included tag for paginator

--- capital/goods/templatetags/tags.py
+++ capital/goods/templatetags/tags.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlencode
 from goods.models import Categories
 from django import template
 
@@ -8,3 +9,8 @@ register = template.Library()
 def tag_categories():
     return Categories.objects.all()
 
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
